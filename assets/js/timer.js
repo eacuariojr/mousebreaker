b54e2309a327810b4dce35affb145f445c9c087e
ErwinA <acuario.erwin00@gmail.com>
2021-09-01 23:29:06 -0700

    Fully functional prototype of game

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,10 +1,19 @@
+function tryStart(){
+    var button = document.querySelector("#clicker button");
+
+    button.setAttribute("onclick", "incrementScore()");
+    startTimer();
+
+    document.querySelector("#counter h1").innerHTML = "1";
+}
+
 function startTimer(){
     //Hardcoded 30 second timer and 60 frames per second
     const TIMER_DURATION = 30000;
     const DISPLAY_FPS = 60;
 
-    var countdown = TIMER_DURATION;
-    var updates = TIMER_DURATION / 1000 * 60;
+    let countdown = TIMER_DURATION;
+    let updates = TIMER_DURATION / 1000 * 60;
 
     let i = updates;
 
@@ -14,14 +23,25 @@ function startTimer(){
             setBarFullness((i/updates) * 100);
             i--;
         }, 1000/DISPLAY_FPS
-    )
+    );
     
-    setTimeout(() => {clearInterval(timer);}, TIMER_DURATION);
+    setTimeout(() => {
+            clearInterval(timer);
+            let button = document.querySelector("#clicker button");
+            button.setAttribute("onclick", "tryStart()");
+        }, TIMER_DURATION
+    );
+}
 
+function incrementScore(){
+    let scoreBoard = document.querySelector("#counter h1");
+    let score = parseInt(scoreBoard.innerText);
+    
+    scoreBoard.innerHTML = score + 1;
 }
 
 function setBarFullness(percent){
-    var bar = document.querySelector("#timer #active-bar");
+    let bar = document.querySelector("#timer #active-bar");
 
     //no input validation because I'm a REBEL
     bar.style.width = percent + "%";
